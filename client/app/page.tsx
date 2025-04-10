d7c0318e0effaed6a93b040daa694e1e6e0c1736
rk11machaba <machabarotshidzwakaizer@gmail.com>
2025-04-10 13:44:53 +0200

    connect to next js

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react';
 
 const Page = () => {
   const [message, setMessage] = useState("Loading");
-  const [items, setItems] = useState([])
+  const [expiringProducts, setExpiringProducts] = useState([]);
+  const [outOfStockProducts, setOutOfStockProducts] = useState([]);
+
 
   useEffect(() => {
     fetch("http://localhost:5000/api/home")
@@ -13,20 +15,31 @@ const Page = () => {
     .then((data)=>{
       // retrieve data
       setMessage(data.message);
-      setItems(data.items);
-
+      setExpiringProducts(data.expiring_products);
+      setOutOfStockProducts(data.quantities);
     });
   }, []);
 
   return(
     <div className='flex flex-col items-center justify-between'>
-      <div className='bg-gray'>{ message }</div>
-      <div>
-        {
-          items.map((item, index) =>(
-            <div key={index}>{ item }</div>
-          ))
-        }
+      <div className='bg-gray-100 p-6 rounded-lg shadow-md text-center'>
+        <h1 className='text-2xl font-bold text-gray-800'>
+          { message }
+        </h1>
+      </div>
+      <div className=''>
+        <h1 className='text-xl font-semibold text-gray-700 mb-4'>Products to Expire</h1>
+        <p className='text-gray-600 bg-red-100 p-4 rounded-lg shadow-sm'>
+          {
+            expiringProducts
+          }
+        </p>
+        <h1 className='text-xl font-semibold text-gray-700 mb-4'>Out of Stock Products</h1>
+        <p className='text-gray-600 bg-red-100 p-4 rounded-lg shadow-sm'>
+          {
+            outOfStockProducts
+          }
+        </p>
       </div>
     </div>
   );
